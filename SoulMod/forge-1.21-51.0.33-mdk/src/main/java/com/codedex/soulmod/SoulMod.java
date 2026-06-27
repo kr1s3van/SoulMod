085d6e5011e141d60a9adbb2c1e1e9566012489b
kris-evan N'guessant <bngue079@uottawa.ca>
2026-06-26 22:49:38 -0400

    Soul Dust and Soul Staff items added

---
+++ SoulMod/forge-1.21-51.0.33-mdk/src/main/java/com/codedex/soulmod/SoulMod.java
@@ -0,0 +1,82 @@
+package com.codedex.soulmod;
+
+import com.codedex.soulmod.item.ModItems;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraft.world.item.CreativeModeTabs;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SoulMod.MOD_ID)
+public class SoulMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "soulmod";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public SoulMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SOUL_DUST);
+        }
+
+        if (event.getTabKey() == CreativeModeTabs.COMBAT) {
+            event.accept(ModItems.SOUL_STAFF);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ SoulMod/forge-1.21-51.0.33-mdk/src/main/java/com/codedex/soulmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.codedex.soulmod.item;
+import com.codedex.soulmod.SoulMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    // On crée le registre d'items. Le MODID doit être "soulmod"
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, SoulMod.MOD_ID);
+
+    // ITEMS
+
+    // Soul Dust
+    public static final RegistryObject<Item> SOUL_DUST = ITEMS.register("soul_dust",
+            () -> new Item(new Item.Properties()));
+
+    // Soul Staff
+    public static final RegistryObject<Item> SOUL_STAFF = ITEMS.register("soul_staff",
+            () -> new Item(new Item.Properties().stacksTo(1)));
+
+
+    // La méthode pour enregistrer tout ça au démarrage du jeu
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
+
+
